5c6c80895e0739140599e25ad6d45e38e2c6e0b4
endsnipes <115029557+endsnipes@users.noreply.github.com>
2023-05-25 22:03:55 +0300

    feat(types) add types for API

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export interface IArticles {
+  id: string;
+  title: string;
+  url: string;
+  imageUrl: string;
+  publishedAt: string;
+  newSite: string;
+  summary: string;
+}
+export interface INews {
+  id: string;
+  title: string;
+  url: string;
+  imageUrl: string;
+  publishedAt: string;
+  newSite: string;
+  summary: string;
+}
+export interface ITab {
+  id: string;
+  label: string;
+}
