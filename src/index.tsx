93d8a405cfa31ea5a65903e6b9483fe44a423620
endsnipes <115029557+endsnipes@users.noreply.github.com>
2023-05-28 09:21:29 +0300

    fix(src):fix App.tsx , index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { App } from "App";
+import { store } from "store";
+import { GlobalStyles } from "ui";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
-
-reportWebVitals();
